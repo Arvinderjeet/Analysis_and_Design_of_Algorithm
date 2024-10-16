d9f42178444ae997e6ca1a025688856f9e68c452
ARVINDERJEET SINGH <arvinderjeet4@gmail.com>
2024-10-16 16:17:16 +0530

    added dummy data to load into objects of class student

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ public:
 
     Student() {
         this->name = "abc";
-        this->roll_no = 0000000000;
+        this->roll_no = 000;
         this->total_marks = 0;
         this->department = "xyz";
     }
@@ -31,7 +31,7 @@ public:
 //    }
     void remove() {
         name.clear();
-        roll_no = 0000000000;
+        roll_no = 000;
         total_marks = 0;
         department.clear();
     }
@@ -48,11 +48,11 @@ public:
     }
 
     void display_data() const {
+        cout << endl << "=================================================" << endl;
         cout << "Name: " << name << endl;
         cout << "Roll no: " << roll_no << endl;
         cout << "Total marks: " << total_marks << endl;
         cout << "Department: " << department << endl;
-        cout << "=================================================";
     }
 
 };
@@ -61,7 +61,72 @@ int main() {
     int choice;
     Student users[100];
     int last_user = 0;
+    string students_names[] = {"Alex Johnson", "Samantha Lee", "Michael Davis", "Emily Turner", "Daniel Smith",
+                             "Olivia Harris", "Matthew Brown", "Sophia Wilson", "James Clark", "Grace Robinson",
+                             "David Martinez", "Isabella Lopez", "Benjamin White", "Mia Gonzalez", "Christopher Scott",
+                             "Abigail Hall", "Jacob Phillips", "Amelia Campbell", "Andrew Walker", "Charlotte Baker",
+                             "Joshua Young", "Avery Adams", "Nicholas Allen", "Harper Torres", "Tyler Brooks",
+                             "Madison Sanders", "Ryan Ramirez", "Evelyn Evans", "Ethan Collins", "Victoria Howard",
+                             "Zachary Kelly", "Lily Patterson", "Nathan Reed", "Chloe Torres", "Adam Gray",
+                             "Ella Peterson", "Jack Cooper", "Scarlett Parker", "Samuel Morgan", "Zoe Morris",
+                             "Logan Hill", "Hannah Wood", "Connor Rivera", "Lucy Ross", "Dylan Powell",
+                             "Layla Jenkins", "Evan Barnes", "Riley Foster", "Isaac Murray", "Aria Hayes"
+    };
+    int students_marks[] = {
+            85, 92, 78, 88, 91,
+            74, 89, 95, 67, 82,
+            90, 79, 84, 76, 93,
+            87, 68, 80, 94, 77,
+            83, 72, 88, 86, 91,
+            75, 82, 89, 73, 90,
+            69, 96, 85, 78, 92,
+            81, 84, 71, 87, 93,
+            88, 66, 95, 77, 70,
+            89, 91, 76, 83, 94
+    };
+    int students_roll_no[] = {
+            425, 893, 601, 754, 319,
+            458, 772, 615, 907, 684,
+            513, 826, 399, 758, 690,
+            847, 574, 832, 642, 521,
+            736, 654, 905, 497, 362,
+            578, 801, 921, 698, 465,
+            367, 682, 737, 856, 731,
+            409, 799, 519, 897, 491,
+            734, 588, 667, 909, 459,
+            777, 613, 838, 453, 947
+    };
 
+    string students_departments[] = {
+            "Computer Science", "Mechanical Engineering", "Electrical Engineering",
+            "Civil Engineering", "Information Technology",
+            "Electronics and Communication", "Chemical Engineering",
+            "Computer Science", "Mechanical Engineering", "Civil Engineering",
+            "Electrical Engineering", "Electronics and Communication", "Information Technology",
+            "Chemical Engineering", "Civil Engineering", "Computer Science",
+            "Mechanical Engineering", "Electrical Engineering", "Electronics and Communication",
+            "Information Technology", "Chemical Engineering", "Civil Engineering",
+            "Computer Science", "Mechanical Engineering", "Electrical Engineering",
+            "Electronics and Communication", "Chemical Engineering", "Information Technology",
+            "Civil Engineering", "Computer Science", "Mechanical Engineering",
+            "Electrical Engineering", "Electronics and Communication", "Chemical Engineering",
+            "Information Technology", "Civil Engineering", "Computer Science",
+            "Mechanical Engineering", "Electrical Engineering", "Information Technology",
+            "Chemical Engineering", "Electronics and Communication", "Civil Engineering",
+            "Computer Science", "Mechanical Engineering", "Electrical Engineering",
+            "Electronics and Communication", "Chemical Engineering","Mechanical Engineering",
+            "Electrical Engineering"
+    };
+
+    int size = sizeof(students_names) / sizeof(students_names[0]);
+
+    for (int i = 0; size > i; ++i) {
+        users[i].name = students_names[i];
+        users[i].roll_no = students_roll_no[i];
+        users[i].total_marks = students_marks[i];
+        users[i].department = students_departments[i];
+        last_user++;
+    }
     do {
         cout << "Main Menu: " << endl;
         cout << "\t1. Add user" << endl;
@@ -88,11 +153,12 @@ int main() {
             for (int i = 0; i < last_user; i++) {
                 users[i].display_data();
             }
+            cout << "=================================================" << endl;
         }
 
         if (choice == 3) {
             int id;
-            cout << "enter roll no. of user to update";
+            cout << "enter roll no. of user to update : ";
             cin >> id;
             for (int i = 0; i < last_user; i++) {
                 if (users[i].roll_no == id) {
